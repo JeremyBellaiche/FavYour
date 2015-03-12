900dca27ad28f6365cadde3ff0bd06180cdbc38a
Yannis Gil <djyannisg@gmail.com>
2015-03-12 20:29:02 +0100

    HEader

--- app/cache/dev/twig/89/b9/908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb10ad7.php
+++ app/cache/dev/twig/89/b9/908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb10ad7.php
@@ -18,6 +18,7 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
         }
 
         $this->blocks = array(
+            'header' => array($this, 'block_header'),
             'body' => array($this, 'block_body'),
         );
     }
@@ -33,9 +34,25 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
     }
 
     // line 4
-    public function block_body($context, array $blocks = array())
+    public function block_header($context, array $blocks = array())
     {
         // line 5
+        echo "\t\t\t<header class=\"intro\">
+\t\t<div class=\"container\">
+\t\t\t<span class=\"brand-name\">
+\t\t\t\tBienvenue sur FavYour
+\t\t\t</span>
+\t\t\t<h1>Vos projets, votre aide</h1>
+\t\t\t<a href=\"#\" class=\"btn btn-cta\"><i class=\"fa fa-chevron-circle-right\"></i> Commencer</a>
+\t\t</div>
+\t</header>
+\t";
+    }
+
+    // line 17
+    public function block_body($context, array $blocks = array())
+    {
+        // line 18
         echo "\t\t<section class=\"projet-semaine\">
 \t\t\t<div class=\"row\">
 \t\t\t\t<div class=\"col-md-1\"></div>
@@ -44,16 +61,16 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
 
 
 \t\t\t        ";
-        // line 12
+        // line 25
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["projects"]) ? $context["projects"] : $this->getContext($context, "projects")));
         foreach ($context['_seq'] as $context["_key"] => $context["project"]) {
-            // line 13
+            // line 26
             echo "\t\t\t\t\t<div class=\"col-md-6\">
 \t\t\t\t\t\t<div>
 \t\t\t\t\t\t\t<div class=\"col-md-4\">
 \t\t\t\t\t\t\t\t<img src=\"";
-            // line 16
+            // line 29
             echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl(("uploads/membres/" . $this->getAttribute($context["project"], "image", array()))), "html", null, true);
             echo "\" alt=\"\" class=\"img-circle img-projet\">
 
@@ -61,7 +78,7 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
 \t\t\t\t\t\t\t\t<div class=\"progress bar-prog-index\">
 \t\t\t\t\t\t\t          <div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"60\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:60%;\">
 \t\t\t\t\t\t\t            ";
-            // line 21
+            // line 34
             echo twig_escape_filter($this->env, (($this->getAttribute($context["project"], "amountWon", array()) / $this->getAttribute($context["project"], "amountRequested", array())) * 100), "html", null, true);
             echo "%
 \t\t\t\t\t\t\t          </div>
@@ -71,13 +88,13 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
 \t\t\t\t\t\t\t</div>
 \t\t\t\t\t\t\t<div class=\"col-md-8\">
 \t\t\t\t\t\t\t\t<h4>";
-            // line 28
+            // line 41
             echo twig_escape_filter($this->env, $this->getAttribute($context["project"], "title", array()), "html", null, true);
             echo "</h4>
 
 \t\t\t\t\t\t\t\t<span>
 \t\t\t\t\t\t\t\t\t";
-            // line 31
+            // line 44
             echo twig_escape_filter($this->env, twig_slice($this->env, $this->getAttribute($context["project"], "description", array()), 0, 250), "html", null, true);
             echo "
 \t\t\t\t\t\t\t\t</span>
@@ -89,7 +106,7 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['project'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 37
+        // line 50
         echo "
 \t\t\t\t</div>
 \t\t\t</div>
@@ -109,6 +126,6 @@ class __TwigTemplate_89b9908929e5cdcaa383644cb3ecebb5d4e1f2e45ded9270c8c8d2871cb
 
     public function getDebugInfo()
     {
-        return array (  93 => 37,  81 => 31,  75 => 28,  65 => 21,  57 => 16,  52 => 13,  48 => 12,  39 => 5,  36 => 4,  11 => 2,);
+        return array (  110 => 50,  98 => 44,  92 => 41,  82 => 34,  74 => 29,  69 => 26,  65 => 25,  56 => 18,  53 => 17,  40 => 5,  37 => 4,  11 => 2,);
     }
 }
